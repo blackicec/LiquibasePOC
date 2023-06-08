d556deb8490b067228740585e94816311a9ca6cb
James Powell <blackicec@gmail.com>
2023-06-08 18:58:03 -0400

    Reduced the timeout for application startup

--- app/src/main/java/app/Application.java
+++ app/src/main/java/app/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan//({"app.services", "app.controllers"})
+@ComponentScan
 @Configuration
 @EnableAutoConfiguration
 @EnableJpaRepositories
@@ -17,7 +17,7 @@ public class Application {
     @SneakyThrows
     public static void main(String[] args) {
         // Giving the DB some time to get out of bed
-        Thread.sleep(8000);
+        Thread.sleep(4000);
 
         SpringApplication.run(Application.class, args);
     }
