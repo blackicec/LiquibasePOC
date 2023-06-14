f543cb430655b20a9b814ea479ed146fbc3e7132
James Powell <blackicec@gmail.com>
2023-06-13 23:23:17 -0400

    Testing out the sqlFile changeset type that runs allow Liquibase to run a sql file as the migration changeset

---
+++ app/src/main/resources/db/views/orders_with_domain-V2.sql
@@ -0,0 +1,10 @@
+DROP VIEW IF EXISTS LiquibaseBuiltMe.v_orders_with_domain;
+CREATE VIEW LiquibaseBuiltMe.v_orders_with_domain
+AS
+    SELECT
+        o.id,
+        o.firstName,
+        o.lastName,
+        o.email
+    FROM orders o
+    WHERE o.email like '%yahoo.com';
